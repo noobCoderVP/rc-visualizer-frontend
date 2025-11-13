1541db53f0208d40f613578b055c6ecfc49c017e
noobcodervp <vaibhavpatel02892@gmail.com>
2025-11-13 19:52:18 +0530

    Increase vocabulary words

--- app/prompts/vocab.ts
+++ app/prompts/vocab.ts
@@ -16,6 +16,93 @@ You must create each question with meaningful context, realistic options, and sh
 1. **Word Selection:**  
    - Randomly choose **10 relevant words** from common CAT RC domains (politics, climate, social issues, finance, governance, philosophy, science, technology etc.).  
    - Prefer words that appear frequently in editorials or exams (e.g., “austerity”, “benevolence”, “subversion”, “ephemeral”, “dissent”).
+   - Consider below list of words as examples (but do not limit to these):
+   🅰️ A
+
+abandon, abate, abdicate, aberrant, abeyance, abhorrent, abide, abjure, abridge, abrogate, absolve, abstain, abstemious, abstract, abstruse, abundant, abysmal, accolade, accord, accrue, acerbic, acme, acquiesce, acrimony, adamant, adept, adhere, adjacent, admonish, adroit, adulation, adulterate, adumbrate, adversity, aesthetic, affable, affluent, aggrandize, aggregate, alacrity, alienate, alleviate, allude, aloof, altercation, altruistic, amalgamate, ambiguous, ambivalent, ameliorate, amenable, amiable, amorphous, anachronism, analogous, anarchy, anathema, ancillary, anecdote, anguish, animosity, anomaly, antagonistic, antecedent, antipathy, antithesis, apathetic, apex, apocryphal, appall, appease, apprehensive, approbation, arbitrary, arcane, archaic, ardent, arduous, articulate, ascetic, aspire, assail, assertive, assiduous, assimilate, assuage, astute, atone, audacious, augment, austere, autonomous, avarice, aver, avid, avow
+
+🅱️ B
+
+baffle, banal, beguile, belabor, bellicose, benevolent, benign, bequeath, berate, beseech, bewilder, blatant, bolster, bombastic, boon, boorish, bourgeois, brash, brazen, brevity, broach, bucolic, burgeon, buttress
+
+🅲 C
+
+cacophony, cajole, callous, candid, cantankerous, capitulate, capricious, captivate, cardinal, caricature, castigate, catalyst, caustic, censure, cerebral, chagrin, charisma, chauvinism, chicanery, chronic, circumspect, circumvent, clandestine, coerce, coherent, cohesive, colloquial, complacent, complement, compliant, concise, conclusive, concomitant, concord, concur, condescending, condone, conducive, conjecture, connive, conscientious, consecrate, consensus, connoisseur, conscientious, consensus, connotation, conspicuous, consternation, constrain, consummate, contentious, contrite, conundrum, convene, conversant, convoluted, copious, cordial, corroborate, cosmopolitan, covert, credulous, cryptic, culpable, cupidity, cursory, curtail
+
+🅳 D
+
+daunt, dearth, debacle, debilitate, decorum, decry, defame, deference, defunct, deleterious, delineate, demagogue, demean, demur, denigrate, denounce, deplete, deplore, depravity, deride, derivative, desiccate, despondent, destitute, desultory, deterrent, detrimental, devious, devoid, didactic, digress, dilapidated, diligent, diminution, discerning, disclose, discord, discrepancy, discrete, discretion, disdain, disheveled, disparage, disparate, dispassionate, dissemble, disseminate, dissent, dissipate, dissonance, distend, divergent, docile, dogged, dogmatic, dormant, dubious, dupe, duplicity
+
+🅴 E
+
+ebullient, eclectic, edify, efface, effervescent, egregious, elaborate, elated, elucidate, elusive, emanate, embellish, eminent, empathy, empirical, emulate, enervate, engender, enhance, enigma, ennui, enormity, enthrall, enumerate, enunciate, ephemeral, epitome, equanimity, equitable, equivocal, erode, erratic, erudite, eschew, esoteric, espouse, ethereal, eulogy, euphemism, exacerbate, exacting, exalt, exasperate, excoriate, exemplary, exhaustive, exhilarate, exigent, exonerate, exorbitant, expedient, explicit, exploit, expound, expunge, extol, extraneous, exuberant
+
+🅵 F
+
+facetious, fallacious, fastidious, fatuous, feasible, fervent, fervor, fickle, figurative, flagrant, flippant, flounder, fluctuate, foible, foment, fortuitous, fractious, frivolous, frugal, fulsome, furtive
+
+🅶 G
+
+gainsay, garrulous, genial, germane, glut, grandiose, gratuitous, gregarious, grievance, guile, gullible
+
+🅷 H
+
+hackneyed, haphazard, harangue, harbinger, hedonist, hegemony, heresy, heterogeneous, hiatus, hierarchy, hindrance, homogeneous, hubris, hyperbole, hypocrisy, hypothetical
+
+🅸 I
+
+iconoclast, idiosyncrasy, ignoble, illicit, immaculate, imminent, immutable, impartial, impeccable, impede, imperative, imperious, impertinent, impetuous, implicit, impromptu, impugn, impunity, inane, inarticulate, incarcerate, incendiary, incessant, incipient, incongruous, incorrigible, incredulous, incumbent, indecisive, indelible, indifferent, indigenous, indignant, indolent, indomitable, ineffable, inept, inert, inevitable, inexorable, infamous, infer, infinitesimal, infirmity, ingenuous, inherent, inimical, innate, innocuous, insatiable, inscrutable, insidious, insipid, insolent, instigate, insular, intangible, integral, intercede, interminable, intermittent, intersperse, intrepid, intrinsic, inundate, invective, inveterate, invigorate, irascible, irate, ironic, irreverent, itinerant
+
+🅹 J
+jaded, jargon, jaunty, jeopardize, jettison, jocular, judicious, juxtapose
+
+🅺 K
+keen, kindle, kinetic, knavery, knack, knead, knell
+
+🅻 L
+laconic, lament, languid, latent, laud, lavish, lethargic, levity, licentious, limpid, listless, loathe, loquacious, lucid, lucrative, lugubrious, luminous
+
+🅼 M
+magnanimous, malevolent, malign, malleable, mar, meander, mellifluous, mendacious, mercenary, meticulous, milieu, mimic, minuscule, mitigate, mollify, morbid, morose, mundane, munificent, myriad
+
+🅽 N
+naive, nebulous, nefarious, negate, negligent, nepotism, nexus, nominal, nonchalant, nostalgia, notoriety, novice, nuance, nullify, nurture
+
+🅾️ O
+obdurate, obfuscate, oblivion, obsequious, obstinate, obstreperous, obtuse, odious, officious, omnipotent, omniscient, onerous, opaque, opportune, opulent, orator, orthodox, ostensible, ostentatious, overt
+
+🅿️ P
+pacify, painstaking, palliate, palpable, panacea, paradigm, paradox, paragon, paramount, parsimonious, partisan, patent, paucity, pedantic, penchant, penitent, pensive, perennial, perfidious, perfunctory, perilous, permeate, pernicious, perpetrate, perpetuate, perspicacious, pertinent, perturb, pervade, petulant, philanthropy, phlegmatic, pious, placate, plausible, plethora, poignant, pompous, pragmatic, precarious, precede, preclude, precocious, predecessor, predilection, preeminent, presumptuous, pretentious, prevalent, prodigal, prodigious, proliferate, propensity, prosaic, protagonist, prototype, provident, provincial, provocative, prudent, puerile, pungent, punitive, purport
+
+🆀 Q
+quaint, qualm, quandary, quell, querulous, quiescent, quintessential, quixotic, quota
+
+🆁 R
+rancor, rapture, ratify, ravenous, rebuff, rebuke, rebut, recalcitrant, recant, receptive, reciprocal, recluse, reconcile, recondite, rectify, redundant, refute, reiterate, relegate, relentless, relevant, relinquish, relish, remiss, remorse, remunerate, renounce, replete, reprehensible, reproach, reprove, repudiate, resilient, resolute, respite, reticent, retract, revere, revile, rhapsody, robust, rudimentary, ruminate
+
+🆂 S
+sagacious, salient, sanctimonious, sanguine, sarcastic, sardonic, satiate, saturate, savant, scrupulous, scrutinize, secular, sedentary, sedition, servile, simulate, skeptical, slander, sluggish, solicit, soliloquy, soluble, somber, sonorous, sophistry, spontaneous, sporadic, spurious, squander, stagnant, static, steadfast, stealthy, stoic, stringent, suave, subdue, subjective, subjugate, sublime, subsequent, subside, subsidize, succinct, superficial, superfluous, supersede, surmise, surreptitious, susceptible, sycophant, synonymous, synthesis, systematic
+
+🆃 T
+tacit, taciturn, tangible, tantamount, tedious, temperate, tenacious, tentative, tenuous, terse, therapeutic, thwart, tirade, torpid, transient, transparent, trepidation, trite, trivial, tumult, turpitude, tyranny
+
+🆄 U
+ubiquitous, ulterior, umbrage, unassuming, unequivocal, unprecedented, unscathed, untenable, utilitarian, utopian
+
+🆅 V
+vacillate, vacuous, vagrant, validate, vehement, venerable, venerate, veracious, verbose, verify, versatile, viable, vindicate, vindictive, virtuoso, virulent, viscous, volatile, voluble, voracious, vulnerable
+
+🆆 W
+wane, wanton, wary, whimsical, wily, winsome, wistful, withstand, wrath
+
+🆇 X
+xenophobia
+
+🆈 Y
+yearn, yoke, yield
+
+🆉 Z
+zeal, zealous, zenith, zephyr
 
 2. **Question Formation:**  
    - For each word, frame either:
